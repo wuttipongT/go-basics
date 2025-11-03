227289977618d8545cea3ad54e827031d9b9ce37
Wuttipong Thongmon <bed.wuttipong@gmail.com>
2025-11-03 22:04:09 +0700

    Making sense of constant values

--- investment_calculator.go
+++ investment_calculator.go
@@ -10,14 +10,17 @@ func main() {
 	// Application entry point
 
 	//fmt.Print("Hello World!")
-
+	const inflationRate = 6.5
 	var investmentAmount float64 = 10000
 	years := 10.0
 	expectedReturnRate := 5.5
 
 	// Calculate future value
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Printf("Future value of the investment: %.2f\n", futureValue)
+	fmt.Printf("Future real value of the investment (adjusted for inflation): %.2f\n", futureRealValue)
 	// To verify whether this is correct or not, opening up some future value calculator online: https://bitl.to/5HQ2
 }
 
