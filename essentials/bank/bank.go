08bc5b5ca53f73eb64b2ac4f9ed2d3d6da6a5835
Wuttipong Thongmon <bed.wuttipong@gmail.com>
2025-11-04 18:59:31 +0700

    Making sense of switch statments

--- essentials/bank/bank.go
+++ essentials/bank/bank.go
@@ -3,11 +3,60 @@ package bank
 import "fmt"
 
 func Run() {
+	var accountBalance = 1000.0
 	// Placeholder for bank application logic
-	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
+
+	for {
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit Money")
+		fmt.Println("3. Withdraw Money")
+		fmt.Println("4. Exit")
+
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		//wantsCheckBalance := choice == 1
+
+		if choice == 1 {
+			fmt.Printf("Your balance is: $%.2f\n", accountBalance)
+		} else if choice == 2 {
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				//return
+				continue
+			}
+
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		} else if choice == 3 {
+			fmt.Print("Withdrawal amount: ")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				return
+			}
+
+			if withdrawalAmount > accountBalance {
+				fmt.Print("Invalid amount. You can't withdraw more than you have.")
+				return
+			}
+
+			accountBalance -= withdrawalAmount
+			fmt.Println("Balance updated! New amount:", accountBalance)
+
+		} else {
+			fmt.Println("Goodbye!")
+			break
+		}
+	}
+	fmt.Println("Thank you for using Go Bank!")
 }
