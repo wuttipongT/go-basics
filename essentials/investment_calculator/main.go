514c33e2d3ef23da21b371e58d167ae7e3a22a71
Wuttipong Thongmon <bed.wuttipong@gmail.com>
2025-11-04 14:26:05 +0700

    Return values & variable scope

--- essentials/investment_calculator/main.go
+++ essentials/investment_calculator/main.go
@@ -6,11 +6,12 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
 	// Application entry point
 
 	//fmt.Print("Hello World!")
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5
@@ -24,15 +25,28 @@ func main() {
 	fmt.Print("Enter the number of years: ")
 	fmt.Scan(&years)
 
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 	// Calculate future value
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	//futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	//futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Printf("Future value of the investment: %.2f\n", futureValue)
 	fmt.Printf("Future real value of the investment (adjusted for inflation): %.2f\n", futureRealValue)
 	// To verify whether this is correct or not, opening up some future value calculator online: https://bitl.to/5HQ2
 
 	// Example output:
+}
+
+func outputText(text string) {
+	fmt.Println(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	// Placeholder for future value calculations
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+
+	return fv, rfv
 }
 
 // go run app.go
