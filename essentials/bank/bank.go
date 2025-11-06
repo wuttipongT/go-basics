3d349497a995c90dfbdc2bbdd6f9e6e3e81c76dd
Wuttipong Thongmon <bed.wuttipong@gmail.com>
2025-11-06 10:45:22 +0700

    Splitting code across files in the same package

--- essentials/bank/bank.go
+++ essentials/bank/bank.go
@@ -45,12 +45,15 @@ func Run() {
 	}
 
 	for {
-		fmt.Println("Welcome to Go Bank!")
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check Balance")
-		fmt.Println("2. Deposit Money")
-		fmt.Println("3. Withdraw Money")
-		fmt.Println("4. Exit")
+
+		presentOptions()
+
+		// fmt.Println("Welcome to Go Bank!")
+		// fmt.Println("What do you want to do?")
+		// fmt.Println("1. Check Balance")
+		// fmt.Println("2. Deposit Money")
+		// fmt.Println("3. Withdraw Money")
+		// fmt.Println("4. Exit")
 
 		var choice int
 		fmt.Print("Your choice: ")
