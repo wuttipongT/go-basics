d038e39f68ad1b07a47629e2b6f923aad6c08b65
Wuttipong Thongmon <bed.wuttipong@gmail.com>
2025-11-07 11:58:55 +0700

    Generics feature

--- main.go
+++ main.go
@@ -21,6 +21,9 @@ func (s str) log() {
 }
 
 func main() {
+	result := add(1, 2)
+	fmt.Println("Intruducing generics: ", result)
+
 	var name str = "Go Basics Application"
 
 	//name = "Go Basics Application"
@@ -87,31 +90,6 @@ func outputText(text string) {
 // 	return a + b;
 // }
 
-func add(a, b interface{}) interface{} {
-
-	aInt, aIsInt := a.(int)
-	bInt, bIsInt := b.(int)
-
-	if aIsInt && bIsInt {
-
-		return aInt + bInt
-	}
-
-	aFloat, aIsFloat := a.(float64)
-	bFloat, bIsFloat := b.(float64)
-
-	if aIsFloat && bIsFloat {
-
-		return aFloat + bFloat
-	}
-
-	aString, aIsString := a.(string)
-	bString, bIsString := b.(string)
-
-	if aIsString && bIsString {
-
-		return aString + bString
-	}
-
-	return nil
+func add[T int | float64 | string](a, b T) T {
+	return a + b // Gennerics
 }
