4410c72e31f30c60da93459da258952801d7b028
Wuttipong Thongmon <bed.wuttipong@gmail.com>
2025-11-08 20:12:37 +0700

    Add a "Users" table

--- lessons/rest-api/routes/events.go
+++ lessons/rest-api/routes/events.go
@@ -58,3 +58,62 @@ func createEvents(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch event id."})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successully"})
+}
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch event id."})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete the event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted succfully!"})
+}
--- lessons/rest-api/routes/routes.go
+++ lessons/rest-api/routes/routes.go
@@ -6,4 +6,7 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)    // GET, POST, PUT, PATCH, DELETE
 	server.GET("/events/:id", getEvent) // events/1, /events/5
 	server.POST("/events", createEvents)
+	server.PUT("/events/:id", updateEvent)
+	server.DELETE("/events/:id", deleteEvent)
+	server.POST("/signup")
 }
